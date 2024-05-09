99a9864c68be41a990fd496cfa50b26e22cecacc
Taiwo Akerele <davcodes24@gmail.com>
2024-05-09 13:28:34 -0700

    made the code easy to read for myself

--- src/components/ModalRegistration.jsx
+++ src/components/ModalRegistration.jsx
@@ -21,12 +21,12 @@ export default function ModalRegistration() {
     localStorage.setItem("email", inputEmail);
     localStorage.setItem("tel", inputTelephone);
     // localStorage.setItem("link", 'github.com/' + linkValue);
-    const changedFirstLetter = linkValue.slice(0,1);
-    const remainingWords = linkValue.slice(1,linkValue.length);
-    const theMainLetterChanged = changedFirstLetter.toLowerCase()
-    const joinedWords = theMainLetterChanged + remainingWords
-    localStorage.setItem('link', 'github.com/' + joinedWords);
-    setIsEmpty(!isEmpty)
+    const changedFirstLetter = linkValue.slice(0, 1);
+    const remainingWords = linkValue.slice(1, linkValue.length);
+    const theMainLetterChanged = changedFirstLetter.toLowerCase();
+    const joinedWords = theMainLetterChanged + remainingWords;
+    localStorage.setItem("link", "github.com/" + joinedWords);
+    setIsEmpty(!isEmpty);
   };
 
   const handleChange = (e) => {
@@ -43,164 +43,169 @@ export default function ModalRegistration() {
   };
   return (
     <>
-      {isClicked === true ? (<>
-        {inputValue === '' || inputEmail === '' || inputTelephone === '' || linkValue === '' === true ? (<>
-          {isEmpty && <EmptyErrorInput message='Fill up the necessary field' />}
-        <div className="bg-white mt-5 p-5 rounded-2xl max-w-[640px] mx-auto h-[525px]">
-        
-            <div className="flex items-center mt-[32px] max-w-[545px] mx-[auto] gap-4">
-            
-              <FormStep stage="1" />
-              <Range />
-              <GrayFormStep stageTwo="2" />
-              <Range />
-              <GrayFormStep stageTwo="3" />
-              <Range />
-              <GrayFormStep stageTwo="4" />
-            </div>
-            <CustomHr />
-            <HeadingPersonal
-              modalHeaderOne="Personal Information"
-              modalParagraph="Please provide your personal details so we can get to know you better."
-            />
-            <form>
-              <div className="flex gap-10 mt-6 ml-6">
-                <div>
-                  <Inputs
-                    labelFor="userName"
-                    labelText="Full Name"
-                    type="text"
-                    value={inputValue}
-                    innerText="John Doe"
-                    handleChange={handleChange}
-                  />
+      {isClicked === true ? (
+        <>
+          {inputValue === "" ||
+          inputEmail === "" ||
+          inputTelephone === "" ||
+          (linkValue === "") === true ? (
+            <>
+              {isEmpty && (
+                <EmptyErrorInput message="Fill up the necessary field" />
+              )}
+              <div className="bg-white mt-5 p-5 rounded-2xl max-w-[640px] mx-auto h-[525px]">
+                <div className="flex items-center mt-[32px] max-w-[545px] mx-[auto] gap-4">
+                  <FormStep stage="1" />
+                  <Range />
+                  <GrayFormStep stageTwo="2" />
+                  <Range />
+                  <GrayFormStep stageTwo="3" />
+                  <Range />
+                  <GrayFormStep stageTwo="4" />
                 </div>
-                <div>
-                  <Inputs
-                    labelFor="Email"
-                    value={inputEmail}
-                    labelText="Email Address"
-                    type="email"
-                    innerText="johndoe24@gmail.com"
-                    handleChange={handleEmail}
-                  />
-                </div>
+                <CustomHr />
+                <HeadingPersonal
+                  modalHeaderOne="Personal Information"
+                  modalParagraph="Please provide your personal details so we can get to know you better."
+                />
+                <form>
+                  <div className="flex gap-10 mt-6 ml-6">
+                    <div>
+                      <Inputs
+                        labelFor="userName"
+                        labelText="Full Name"
+                        type="text"
+                        value={inputValue}
+                        innerText="John Doe"
+                        handleChange={handleChange}
+                      />
+                    </div>
+                    <div>
+                      <Inputs
+                        labelFor="Email"
+                        value={inputEmail}
+                        labelText="Email Address"
+                        type="email"
+                        innerText="johndoe24@gmail.com"
+                        handleChange={handleEmail}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-10 mt-6 ml-6">
+                    <div>
+                      <Inputs
+                        labelFor="tel"
+                        labelText="Phone Number"
+                        type="tel"
+                        value={inputTelephone}
+                        innerText="+91-1234-0890"
+                        handleChange={handleTelephone}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <Inputs
+                        labelFor="link"
+                        labelText="Portfolio/GitHub Link"
+                        type="text"
+                        value={linkValue}
+                        innerText="github.com/taiwosoftware"
+                        handleChange={handleLink}
+                      />
+                    </div>
+                  </div>
+                  <CustomHr />
+                  <Link href="">
+                    <button
+                      onClick={handleClick}
+                      className="text-white w-[110px] ml-[460px] h-[41px] text-center mt-6  rounded-[10px] summit"
+                      type="submit"
+                    >
+                      Next Step
+                    </button>
+                  </Link>
+                </form>
               </div>
-              <div className="flex gap-10 mt-6 ml-6">
-                <div>
-                  <Inputs
-                    labelFor="tel"
-                    labelText="Phone Number"
-                    type="tel"
-                    value={inputTelephone}
-                    innerText="+91-1234-0890"
-                    handleChange={handleTelephone}
-                  />
+            </>
+          ) : (
+            <>
+              <div className="bg-white mt-5 p-5 rounded-2xl max-w-[640px] mx-auto h-[525px]">
+                <div className="flex items-center mt-[32px] max-w-[545px] mx-[auto] gap-4">
+                  <FormStep stage="1" />
+                  <Range />
+                  <GrayFormStep stageTwo="2" />
+                  <Range />
+                  <GrayFormStep stageTwo="3" />
+                  <Range />
+                  <GrayFormStep stageTwo="4" />
                 </div>
-                <div className="mb-4">
-                  <Inputs
-                    labelFor="link"
-                    labelText="Portfolio/GitHub Link"
-                    type="text"
-                    value={linkValue}
-                    innerText="github.com/taiwosoftware"
-                    handleChange={handleLink}
-                  />
-                </div>
+                <CustomHr />
+                <HeadingPersonal
+                  modalHeaderOne="Personal Information"
+                  modalParagraph="Please provide your personal details so we can get to know you better."
+                />
+                <form>
+                  <div className="flex gap-10 mt-6 ml-6">
+                    <div>
+                      <Inputs
+                        labelFor="userName"
+                        labelText="Full Name"
+                        type="text"
+                        value={inputValue}
+                        innerText="John Doe"
+                        handleChange={handleChange}
+                      />
+                    </div>
+                    <div>
+                      <Inputs
+                        labelFor="Email"
+                        value={inputEmail}
+                        labelText="Email Address"
+                        type="email"
+                        innerText="johndoe24@gmail.com"
+                        handleChange={handleEmail}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-10 mt-6 ml-6">
+                    <div>
+                      <Inputs
+                        labelFor="tel"
+                        labelText="Phone Number"
+                        type="tel"
+                        value={inputTelephone}
+                        innerText="+91-1234-0890"
+                        handleChange={handleTelephone}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <Inputs
+                        labelFor="link"
+                        labelText="Portfolio/GitHub Link"
+                        type="text"
+                        value={linkValue}
+                        innerText="github.com/taiwosoftware"
+                        handleChange={handleLink}
+                      />
+                    </div>
+                  </div>
+                  <CustomHr />
+                  <Link href="/two">
+                    <button
+                      onClick={handleClick}
+                      className="text-white w-[110px] ml-[460px] h-[41px] text-center mt-6  rounded-[10px] summit"
+                      type="submit"
+                    >
+                      Next Step
+                    </button>
+                  </Link>
+                </form>
               </div>
-              <CustomHr />
-               <Link href=''>
-               <button
-                  onClick={handleClick}
-                  className="text-white w-[110px] ml-[460px] h-[41px] text-center mt-6  rounded-[10px] summit"
-                  type="submit"
-                >
-                  Next Step
-                </button>
-               </Link>
-              
-            </form>
-          </div> 
-
-        </>) : (<>
+            </>
+          )}
+        </>
+      ) : (
+        <>
           <div className="bg-white mt-5 p-5 rounded-2xl max-w-[640px] mx-auto h-[525px]">
-        
-        <div className="flex items-center mt-[32px] max-w-[545px] mx-[auto] gap-4">
-        
-          <FormStep stage="1" />
-          <Range />
-          <GrayFormStep stageTwo="2" />
-          <Range />
-          <GrayFormStep stageTwo="3" />
-          <Range />
-          <GrayFormStep stageTwo="4" />
-        </div>
-        <CustomHr />
-        <HeadingPersonal
-          modalHeaderOne="Personal Information"
-          modalParagraph="Please provide your personal details so we can get to know you better."
-        />
-        <form>
-          <div className="flex gap-10 mt-6 ml-6">
-            <div>
-              <Inputs
-                labelFor="userName"
-                labelText="Full Name"
-                type="text"
-                value={inputValue}
-                innerText="John Doe"
-                handleChange={handleChange}
-              />
-            </div>
-            <div>
-              <Inputs
-                labelFor="Email"
-                value={inputEmail}
-                labelText="Email Address"
-                type="email"
-                innerText="johndoe24@gmail.com"
-                handleChange={handleEmail}
-              />
-            </div>
-          </div>
-          <div className="flex gap-10 mt-6 ml-6">
-            <div>
-              <Inputs
-                labelFor="tel"
-                labelText="Phone Number"
-                type="tel"
-                value={inputTelephone}
-                innerText="+91-1234-0890"
-                handleChange={handleTelephone}
-              />
-            </div>
-            <div className="mb-4">
-              <Inputs
-                labelFor="link"
-                labelText="Portfolio/GitHub Link"
-                type="text"
-                value={linkValue}
-                innerText="github.com/taiwosoftware"
-                handleChange={handleLink}
-              />
-            </div>
-          </div>
-          <CustomHr />
-           <Link href='/two'>
-           <button
-              onClick={handleClick}
-              className="text-white w-[110px] ml-[460px] h-[41px] text-center mt-6  rounded-[10px] summit"
-              type="submit"
-            >
-              Next Step
-            </button>
-           </Link>
-          
-        </form>
-      </div> 
-        </>)}
-      </>) : (<>
-        <div className="bg-white mt-5 p-5 rounded-2xl max-w-[640px] mx-auto h-[525px]">
             <div className="flex items-center mt-[32px] max-w-[545px] mx-[auto] gap-4">
               <FormStep stage="1" />
               <Range />
@@ -261,18 +266,17 @@ export default function ModalRegistration() {
                 </div>
               </div>
               <CustomHr />
-                <button
-                  onClick={handleClick}
-                  className="text-white w-[110px] ml-[460px] h-[41px] text-center mt-6  rounded-[10px] summit"
-                  type="submit"
-                >
-                  Next Step
-                </button>
-              
+              <button
+                onClick={handleClick}
+                className="text-white w-[110px] ml-[460px] h-[41px] text-center mt-6  rounded-[10px] summit"
+                type="submit"
+              >
+                Next Step
+              </button>
             </form>
-          </div> 
-      </>)}
+          </div>
+        </>
+      )}
     </>
   );
 }
-
